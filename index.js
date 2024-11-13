c34ae455110af1f53a7a37014794ebbc8cf44bb1
Boo-coder <Bouna112@yahoo.com>
2024-11-12 23:42:39 -0500

    arrw fnct

---
+++ index.js
@@ -0,0 +1,11 @@
+const divide = function(a, b) { 
+    return a / b;
+}
+console.log(2000 / 100);
+
+const square = (x) => x ** 2;
+
+const add = function (a, b) {
+    return a + b;
+}
+console.log(add(3, 4));
